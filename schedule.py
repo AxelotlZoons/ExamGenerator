25b4cfecdcf4467f9914f20bc6424a7331046b59
AxelotlZoons <axelzoons.dev@gmail.com>
2025-01-03 17:35:49 +0100

    added schedule file

---
+++ schedule.py
@@ -0,0 +1,54 @@
+"""This is an overview of the sections taught per lecture week for the academic
+year 2023-2024. """
+
+week0 = []
+week1 = [
+# "preliminaries.tex", This only contains questions for the year 2425, etc.
+"exp_poisson.tex",
+"exponential_wins.tex",
+]
+
+week2 = [
+"probabilistic_arithmetic.tex",
+"constructiondiscretetime.tex",
+"simulation_psychiatrists.tex",
+"construction_inventory.tex",
+]
+
+week3 = [
+"simulation_inventory.tex",
+"simulationsdiscrete.tex",
+]
+
+week4 = [
+"constructioncontinuoustime.tex",
+"waiting_time_distribution.tex",
+"simulations-continuous.tex",
+"discrete-event-simulation.tex",
+]
+
+week5 = [
+"kendall.tex",
+"ratestability.tex",
+"limitingperfmeasures.tex",
+"renewal_reward.tex",
+"little.tex",
+"pasta.tex",
+]
+
+week6 = [
+"levelcrossing.tex",
+"mm1.tex",
+"mnmn1_applications.tex",
+"mxm1_pk.tex",
+"n_policies_mg1.tex",
+]
+
+week7 = [
+"inventory_analysis.tex",
+"sakasegawa.tex",
+"failures.tex",
+"tandem.tex",
+]
+
+weeks = [week0, week1, week2, week3, week4, week5, week6, week7]
